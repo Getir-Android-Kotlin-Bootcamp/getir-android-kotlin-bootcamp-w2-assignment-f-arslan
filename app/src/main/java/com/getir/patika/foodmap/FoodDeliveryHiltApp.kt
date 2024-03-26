b0839cd98e3471e6d5f697c774b9571db638b1a4
f-arslan <buildsmstff@gmail.com>
2024-03-26 17:33:21 +0300

    feat: binding module for hilt

---
+++ app/src/main/java/com/getir/patika/foodmap/FoodDeliveryHiltApp.kt
@@ -0,0 +1,7 @@
+package com.getir.patika.foodmap
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FoodDeliveryHiltApp: Application()
---
+++ app/src/main/java/com/getir/patika/foodmap/data/di/DataModule.kt
@@ -0,0 +1,66 @@
+package com.getir.patika.foodmap.data.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import com.getir.patika.foodmap.R
+import com.getir.patika.foodmap.data.LocationRepository
+import com.getir.patika.foodmap.data.PreferencesRepository
+import com.getir.patika.foodmap.data.impl.LocationDataSource
+import com.getir.patika.foodmap.data.impl.PreferencesDataSource
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.net.PlacesClient
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataModule {
+    @Singleton
+    @Provides
+    fun providePlacesClient(@ApplicationContext context: Context): PlacesClient {
+        Places.initialize(context, context.getString(R.string.api_key))
+        return Places.createClient(context)
+    }
+
+    @Singleton
+    @Provides
+    fun providePreferencesDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create(
+            corruptionHandler = ReplaceFileCorruptionHandler(
+                produceNewData = { emptyPreferences() }
+            ),
+            scope = CoroutineScope(Dispatchers.IO + SupervisorJob()),
+            produceFile = { context.preferencesDataStoreFile(USER_PREFERENCES_NAME) }
+        )
+    }
+
+    @Singleton
+    @Provides
+    fun provideIODispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    private const val USER_PREFERENCES_NAME = "user_preferences"
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface DataModuleBinder {
+    @Binds
+    fun bindPreferencesRepository(impl: PreferencesDataSource): PreferencesRepository
+
+    @Binds
+    fun bindLocationRepository(impl: LocationDataSource): LocationRepository
+}
